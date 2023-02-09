7db0847b54186e4ed36ef7110d7915890866f657
COrrRoq <catherine.orr@roq.co.uk>
2023-02-09 16:03:01 +0000

    Team commit

--- game.js
+++ game.js
@@ -1,38 +1,70 @@
 // A classic game of rock, paper, scissors
+var prompt = require('prompt-sync')
+
+let choice = '';
+let roll = '';
 
 class Game {
 
     constructor() {
+        this.computerInput = 0;
+        this.userInput = 0;
         // Key: 0 - Rock, 1 - Paper, 2 - Scissors
     }
 
+
     generateRandNo() {
-        // Generate a random integer between 0 and 2.  
-        // Each number represents either rock, paper or scissors - check against key.
-        // Return the randomly generated integer, store this as computerInput
-
-        throw new Error('generateRandNo functionality not yet supported!');
+        let randNo = Math.floor(Math.random() * 3);
+        this.computerInput = randNo;
+        return randNo;
     }
 
-    userMove() {
-        // Give the user a prompt, ask for their move - rock, paper or scissors. 
-        // Convert the user input to an integer - e.g. if user input = rock, then convert to 0.
-        // Return the integer after converting, store this as userInput
+// Generate a random integer between 0 and 2.  
+// Each number represents either rock, paper or scissors - check against key.
+// Return the randomly generated integer, store this as computerInput
 
-        throw new Error('userMove functionality not yet supported!'); 
+
+
+userMove() {
+    userInput = prompt('Rock, paper, scissors?');
+
+    if (userInput == 'ROCK') {
+        userInput = 0
+        return userInput
     }
-
-    checkScore(computerInput, userInput) {
-        // Step 1 - Check the user’s move compared with the computer’s generated move and provide a winner - output the winner - e.g. Winner is Computer, or Winner is User.
-
-        // Step 2 - Include a counter for the games played - output how many games each player (Computer and User) has won after each game - e.g. Computer has won 5, User has won 3, out of 8 games.
-    
-        // Inputs: computerInput = the computer's move, userInput = the user's move
-
-        throw new Error('checkScore functionality not yet supported!');
+    else if (userInput == 'PAPER') {
+        userInput = 1
+        return userInput
+    } else if (userInput == 'SCISSORS')
+    {
+        userInput = 2
+        return userInput
     }
-    
-    //p.s if this is too easy - expand the game and add lizard, spock to the mix.
 }
+// Give the user a prompt, ask for their move - rock, paper or scissors. 
+// Convert the user input to an integer - e.g. if user input = rock, then convert to 0.
+// Return the integer after converting, store this as userInput
+
+
+
+checkScore(){
+
+    if (userInput == computerInput){
+        console.log('DRAW!!!')
+    }
+    else if (userInput == 0 && computerInput == 2) {
+        console.log('You win!')
+    }
+    else if (userInput == 0 && computerInput == 1) {
+        console.log('You lose!')
+}
+}
+}
+// Step 1 - Check the user’s move compared with the computer’s generated move and provide a winner - output the winner - e.g. Winner is Computer, or Winner is User.
+
+// Step 2 - Include a counter for the games played - output how many games each player (Computer and User) has won after each game - e.g. Computer has won 5, User has won 3, out of 8 games.
+
+// Inputs: computerInput = the computer's move, userInput = the user's move
+
 
 module.exports = Game;
